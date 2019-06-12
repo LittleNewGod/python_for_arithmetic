d5a57d7b945d31b3cdf9ee8a06e710abf40d8231
LittleNewGod <liuqingxin810@163.com>
2019-06-12 21:11:18 +0800

    day100-打卡

---
+++ 力扣算法练习/day100-二叉搜索树中第K小的元素.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-#
+#-------------------------------------------------------------------------------
+# Name:         day100-二叉搜索树中第K小的元素
+# Author:       xin
+# Date:         2019/6/12
+# IDE:  PyCharm
+# -------------------------------------------------------------------------------
+
+
+# 给定一个二叉搜索树，编写一个函数 kthSmallest 来查找其中第 k 个最小的元素。
+#
+# 说明：
+# 你可以假设 k 总是有效的，1 ≤ k ≤ 二叉搜索树元素个数。
+#
+# 示例 1:
+#
+# 输入: root = [3,1,4,null,2], k = 1
+#    3
+#   / \
+#  1   4
+#   \
+#    2
+# 输出: 1
+# 示例 2:
+#
+# 输入: root = [5,3,6,2,4,null,null,1], k = 3
+#        5
+#       / \
+#      3   6
+#     / \
+#    2   4
+#   /
+#  1
+# 输出: 3
+# 进阶：
+# 如果二叉搜索树经常被修改（插入/删除操作）并且你需要频繁地查找第 k 小的值，你将如何优化 kthSmallest 函数？
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+#解法一:
+class Solution:
+    def kthSmallest(self, root, k):
+        def gen(r):
+            if r is not None:
+                yield from gen(r.left)
+                yield r.val
+                yield from gen(r.right)
+
+        it = gen(root)
+        for _ in range(k):
+            ans = next(it)
+        return ans
+
+#解法二:
+# class Solution(object):
+#     def kthSmallest(self, root, k):
+#         """
+#         :type root: TreeNode
+#         :type k: int
+#         :rtype: int
+#         """
+#         arr = []
+#         self.get_tree_arr(root, arr)
+#         # print(arr)
+#         return arr[k - 1]
+#
+#     def get_tree_arr(self, root, arr):
+#         if root is None:
+#             return
+#         self.get_tree_arr(root.left, arr)
+#         arr.append(root.val)
+#         self.get_tree_arr(root.right, arr)
